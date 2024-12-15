ad03ada09464b3cd603af2b07de04febe7180680
Vanisper <273266469@qq.com>
2024-12-16 05:41:50 +0800

    feat(packages\components): 更新 Vite 配置，设置基础路径为 './'，便于github-pages的部署访问

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -4,6 +4,7 @@ import uni from '@dcloudio/vite-plugin-uni'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
