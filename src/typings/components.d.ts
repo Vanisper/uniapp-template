95d0c80aa408ec6b96364508af87666cb849c654
Vanisper <273266469@qq.com>
2026-01-22 02:59:58 +0800

    feat: 新增 HiCounter 组件

--- src/typings/components.d.ts
+++ src/typings/components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./../components/AppFooter.vue')['default']
     AppLogos: typeof import('./../components/AppLogos.vue')['default']
+    HiCounter: typeof import('./../components/HiCounter.vue')['default']
     InputEntry: typeof import('./../components/InputEntry.vue')['default']
   }
 }
