ab094bd61056b3629dc65acc155efe10d5813f01
Vanisper <273266469@qq.com>
2026-09-12 00:16:02 +0800

    chore(deps): 升级 ESLint 10 及兼容配置组
    显式安装新版 uni-helper 要求的 antfu 配置，并将自定义规则放在后续配置中，保留原有单行模板规则。

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import uniHelper from '@uni-helper/eslint-config'
 
-export default uniHelper({
+export default uniHelper({}, {
   rules: {
     'vue/singleline-html-element-content-newline': 'off',
   },
